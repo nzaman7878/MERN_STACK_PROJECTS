fcb7103d643c947d8642f2619e7ece976102439c
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-08-15 01:44:27 +0530

    feat: add nodemon as a devDependency and update package.json structure

--- task-manager/server/server.js
+++ task-manager/server/server.js
@@ -6,7 +6,19 @@ app.get("/", (req, res)=> {
     res.send("Hello from express");
 });
 
+app.get("/api/tasks", (req, res)=>{
+    res.json([
+        {
+            id :1,
+            title: "Learn Node.js",
 
+        },
+        {
+            id :2,
+            title : "Learn Express.js"
+        }
+    ])
+})
 
 app.listen(5000, ()=>{
     console.log("Server is running on port 5000");
