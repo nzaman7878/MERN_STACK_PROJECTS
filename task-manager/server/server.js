4d7aba5b1ecf84b7d9a968960a718e47daaee68f
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-08-15 00:51:31 +0530

    feat: implement basic HTTP server with request handling

--- task-manager/server/server.js
+++ task-manager/server/server.js
@@ -1,1 +1,10 @@
-console.log("Server starting...");
+import http from "http";
+
+const server = http.createServer((req, res) => {
+    res.end("Hello from the server");
+
+})
+
+server.listen(5000, ()=> {
+    console.log("Server is listening on port 5000...");
+})
