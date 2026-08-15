134a1eb6304b1246bafe0e44688e0b1bc622170b
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-08-15 15:09:47 +0530

    feat: implement detailed task retrieval with 404 error handling

--- task-manager/server/server.js
+++ task-manager/server/server.js
@@ -23,6 +23,18 @@ app.get("/api/tasks", (req, res)=>{
     res.status(200).json(tasks);
 })
 
+app.get("/api/tasks/:id", (req, res)=>{
+    const tasksId = Number(req.params.id);
+    const task = tasks.find(tasks => tasks.id === tasksId);
+
+    if (!task){
+        res.status(404).json({
+            message: "Task not found"
+        });
+    }
+res.status(200).json(task);
+})
+
 app.listen(5000, ()=>{
     console.log("Server is running on port 5000");
 })
