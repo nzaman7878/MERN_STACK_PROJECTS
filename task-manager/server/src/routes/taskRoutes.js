df6f50607d5088bb055d597bde4abacead92517b
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-08-19 21:55:23 +0530

    feat: add authentication protection to task routes

--- task-manager/server/src/routes/taskRoutes.js
+++ task-manager/server/src/routes/taskRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import { getTasks, createTask, getTask, updateTask, deleteTask  } from '../controllers/taskController.js';
+import  protect  from '../middleware/authMiddleware.js';
+
 const router = express.Router();
 
-router.get("/", getTasks);
-router.post("/", createTask);
-router.get("/:id", getTask);
-router.patch("/:id", updateTask); 
-router.delete("/:id", deleteTask);
+router.get("/", protect, getTasks);
 
+router.post("/", protect, createTask);
+
+router.get("/:id", protect, getTask);
+
+router.patch("/:id", protect, updateTask);
+
+router.delete("/:id", protect, deleteTask);
 export default router;
