94d8a777f12d605a772f0e435649e258806ceafb
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-08-20 23:27:06 +0530

    feat: create TaskCard component and update App to include it

--- task-manager/client/src/App.jsx
+++ task-manager/client/src/App.jsx
@@ -1,14 +1,36 @@
-
-import './App.css'
+import TaskCard from "./components/TaskCard";
 
 function App() {
+    const tasks = [
+        {
+            id: 1,
+            title: "Learn React",
+            completed: false
+        },
+        {
+            id: 2,
+            title: "Learn Express",
+            completed: true
+        }
+    ];
 
+    return (
+        <div>
+            <h1>Task Manager</h1>
 
-  return (
-    <>
-     <h1>Task Manager</h1>
-    </>
-  )
+            {tasks.map((task) => (
+                <div key={task.id}>
+                    <h3>{task.title}</h3>
+
+                    <p>
+                        {task.completed
+                            ? "Completed"
+                            : "Pending"}
+                    </p>
+                </div>
+            ))}
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ task-manager/client/src/components/TaskCard.jsx
@@ -0,0 +1,10 @@
+function TaskCard() {
+    return (
+        <div>
+            <h3>Learn React</h3>
+            <button>Delete</button>
+        </div>
+    );
+}
+
+export default TaskCard;
