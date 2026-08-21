9fdcb4b51a8fe39e84306b28887d59ea13c7fd5e
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-08-21 22:38:01 +0530

    feat: refactor App component to fetch tasks from API and remove hardcoded tasks

--- task-manager/client/src/App.jsx
+++ task-manager/client/src/App.jsx
@@ -1,32 +1,26 @@
-import TaskCard from "./components/TaskCard";
+import { useEffect, useState } from "react";
+import api from "./services/api";
 
 function App() {
-    const tasks = [
-        {
-            id: 1,
-            title: "Learn React",
-            completed: false
-        },
-        {
-            id: 2,
-            title: "Learn Express",
-            completed: true
-        }
-    ];
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            const response = await api.get("/tasks");
+
+            setTasks(response.data);
+        };
+
+        fetchTasks();
+    }, []);
 
     return (
         <div>
             <h1>Task Manager</h1>
 
             {tasks.map((task) => (
-                <div key={task.id}>
+                <div key={task._id}>
                     <h3>{task.title}</h3>
-
-                    <p>
-                        {task.completed
-                            ? "Completed"
-                            : "Pending"}
-                    </p>
                 </div>
             ))}
         </div>
