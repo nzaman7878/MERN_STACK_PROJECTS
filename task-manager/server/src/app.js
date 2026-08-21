9b2afaac8912f94d659b411c538d207cd1ff2f5d
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-08-21 22:42:21 +0530

    feat: add cors middleware and update package.json and package-lock.json to include cors dependency

--- task-manager/server/src/app.js
+++ task-manager/server/src/app.js
@@ -2,11 +2,19 @@ import express from "express";
 import taskRoutes from "./routes/taskRoutes.js";
 import errorHandler from "./middleware/errorMiddleware.js";
 import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(errorHandler);
+
+app.use(
+    cors({
+        origin: "http://localhost:5173"
+    })
+);
+
 
 app.use("/api/tasks", taskRoutes);
 
