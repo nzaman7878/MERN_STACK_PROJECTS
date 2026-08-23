9e6ba57c1e904243276a73e1cff02ff6367a4bc5
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-08-23 23:22:06 +0530

    feat: add loginUser function to authService for user authentication

--- task-manager/client/src/Services/authService.js
+++ task-manager/client/src/Services/authService.js
@@ -3,3 +3,6 @@ import api from "./api";
 export const registerUser = (userData) => {
     return api.post("/auth/register", userData);
 };
+export const loginUser = (userData) => {
+    return api.post("/auth/login", userData);
+};
