97aaeaaf33cd88cd601787cbbf38fa8e1bb0d23c
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-08-25 01:04:16 +0530

    feat: implement ProtectedRoute component for route protection

--- task-manager/client/src/App.jsx
+++ task-manager/client/src/App.jsx
@@ -1,18 +1,17 @@
-import { useEffect, useState } from "react";
-import { getTasks } from "./Services/taskService.js";
-import TaskForm from "./components/TaskForm";
-import TaskCard from "./components/TaskCard";
 import { Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-
-    <Routes>
-            <Route path="/" element={<Login />} />
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={<Login />}
+            />
 
             <Route
                 path="/register"
@@ -21,68 +20,13 @@ function App() {
 
             <Route
                 path="/dashboard"
-                element={<Dashboard />}
+                element={
+                    <ProtectedRoute>
+                        <Dashboard />
+                    </ProtectedRoute>
+                }
             />
         </Routes>
-
-    const [tasks, setTasks] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState("");
-
-    useEffect(() => {
-        const fetchTasks = async () => {
-            try {
-                setLoading(true);
-
-                const response = await getTasks();
-
-                setTasks(response.data);
-
-            } catch (error) {
-                setError(
-                    error.response?.data?.message ||
-                    "Failed to load tasks"
-                );
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchTasks();
-    }, []);
-
-    const handleTaskCreated = (newTask) => {
-        setTasks((currentTasks) => [
-            newTask,
-            ...currentTasks
-        ]);
-    };
-
-    if (loading) {
-        return <p>Loading tasks...</p>;
-    }
-
-    return (
-        <div>
-            <h1>Task Manager</h1>
-
-            <TaskForm
-                onTaskCreated={handleTaskCreated}
-            />
-
-            {error && <p>{error}</p>}
-
-            {tasks.length === 0 ? (
-                <p>No tasks yet. Create your first task.</p>
-            ) : (
-                tasks.map((task) => (
-                    <TaskCard
-                        key={task._id}
-                        task={task}
-                    />
-                ))
-            )}
-        </div>
     );
 }
 
