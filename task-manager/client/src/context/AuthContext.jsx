227d6e4631ffa0cf0c33d4fcd253550015529c60
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-08-26 23:39:28 +0530

    feat: enhance AuthContext with user and token state initialization from localStorage

--- task-manager/client/src/context/AuthContext.jsx
+++ task-manager/client/src/context/AuthContext.jsx
@@ -1,19 +1,53 @@
-import { createContext, useContext, useState } from "react";
+import {
+    createContext,
+    useContext,
+    useState
+} from "react";
 
 const AuthContext = createContext();
 
 function AuthProvider({ children }) {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(() => {
+        const savedUser = localStorage.getItem("user");
+
+        return savedUser
+            ? JSON.parse(savedUser)
+            : null;
+    });
+
+    const [token, setToken] = useState(() => {
+        return localStorage.getItem("token");
+    });
+
+    const login = (userData, accessToken) => {
+        setUser(userData);
+        setToken(accessToken);
+
+        localStorage.setItem(
+            "user",
+            JSON.stringify(userData)
+        );
+
+        localStorage.setItem(
+            "token",
+            accessToken
+        );
+    };
 
     const logout = () => {
-    setUser(null);
-};
+        setUser(null);
+        setToken(null);
+
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+    };
 
     return (
         <AuthContext.Provider
             value={{
                 user,
-                setUser,
+                token,
+                login,
                 logout
             }}
         >
